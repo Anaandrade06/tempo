1306622ee3de33100bb2f067cc74b34e13bcdce4
Clima <anaelisaandrade65@gmail.com>
2024-04-16 11:52:00 -0300

    começo de tudo

---
+++ Clima/result.cs
@@ -0,0 +1,15 @@
+using System.Dynamic;
+
+public class results 
+ {
+    public int tempo {get;set;}
+    public string description {get;set;} 
+    public string city {get;set;}
+    public double rain {get;set;}
+    public int humidity {get;set;}
+    public string sunrise {get;set;}
+    public string sunset {get;set;}
+    public string wind_speed {get;set;}
+    public string wind_direction {get;set;}
+    public string moon_phase {get;set;}
+    }
