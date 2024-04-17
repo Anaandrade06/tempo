e96712cb17531b6845f5171781a40ac5c480911a
clima <anaelisaandrade65@gmail.com>
2024-04-17 11:51:12 -0300

    Grid e Frame

--- Clima/result.cs
+++ Clima/result.cs
@@ -1,15 +1,15 @@
 using System.Dynamic;
 
-public class results 
+public class Results 
  {
-    public int tempo {get;set;}
-    public string description {get;set;} 
-    public string city {get;set;}
-    public double rain {get;set;}
-    public int humidity {get;set;}
-    public string sunrise {get;set;}
-    public string sunset {get;set;}
-    public string wind_speed {get;set;}
-    public string wind_direction {get;set;}
-    public string moon_phase {get;set;}
+    public int Tempo {get;set;}
+    public string Description {get;set;} 
+    public string City {get;set;}
+    public double Rain {get;set;}
+    public int Humidity {get;set;}
+    public string Sunrise {get;set;}
+    public string Sunset {get;set;}
+    public string Wind_speed {get;set;}
+    public string Wind_direction {get;set;}
+    public string Moon_phase {get;set;}
     }
