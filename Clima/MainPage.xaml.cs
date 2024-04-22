b2f7db40dd797d998f715e75cf2ab044f678c7b4
tempo <anaelisaandrade652gmail.com>
2024-04-22 11:48:59 -0300

    corrigi alguns erros

--- Clima/MainPage.xaml.cs
+++ Clima/MainPage.xaml.cs
@@ -12,5 +12,11 @@ void TestarLayout()
 {
 	results.Temp=23;
 }
+void PreencherTela()
+{
+  LabelTemp.Text= results.Temp. ToString();
+  LabelCity.Text= results.City;  
+  if ( results.Currently=="dia");
+}
 
 }
