09b1a0e41678b59a5e09cb7e156c798dc0cb70ce
tempo <anaelisaadrade65@gmail.com>
2024-04-25 09:07:23 -0300

    algumas poucas coisas

--- Clima/MainPage.xaml.cs
+++ Clima/MainPage.xaml.cs
@@ -7,17 +7,19 @@ const string Url="HPPTS://api.hgbrasil.com/eather? woeid=455927&key=3091de3d";
 	{
 		InitializeComponent();
 	}
-   Results results = new Results();
-
+   Respota resposta;
+}
 void TestarLayout()
 {
 	results.Temp=23;
 }
 void PreencherTela()
 {
-  LabelTemp.Text= results.Temp. ToString();
-  LabelCity.Text= results.City;  
-  if ( results.Currently=="dia");
-}
-
+  LabelTemp.Text= resposta.results.Temp. ToString();
+  LabelCity.Text= resposta.results.City;
+  LabelDescription.Text= resposta.results.Description.ToString();
+  LabelRain .Text= resposta.results.Rain;
+  LabelSunrise.Text= resposta.results.Sunrise;
+  LabelHumidity.Text= resposta.results.Humidity;
+   LabelHumidity.Text= resposta.results.Humidity;
 }
