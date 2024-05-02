7e3427fb5065a21c6bfcc5fb24e268f973267f41
clima <anaelisaandrade65@gmai.com>
2024-05-02 09:03:11 -0300

    quase lá

--- Clima/MainPage.xaml.cs
+++ Clima/MainPage.xaml.cs
@@ -1,25 +1,51 @@
-﻿namespace Clima;
+﻿using System.Text.Json;
+namespace Clima;
 
 public partial class MainPage : ContentPage
 {
-const string Url="HPPTS://api.hgbrasil.com/eather? woeid=455927&key=3091de3d";
+   Resposta resposta;
+  const string Url="HPPTS://api.hgbrasil.com/eather? woeid=455927&key=3091de3d";
 	public MainPage()
 	{
 		InitializeComponent();
+      AtualizaTempo();
 	}
-   Respota resposta;
-}
-void TestarLayout()
-{
-	results.Temp=23;
-}
+async void AtualizaTempo()
+   {
+      try 
+      {
+      
+         var httpClient =new HttpClient();
+         var response = await httpClient.GetAsync(Url);
+         if (response.IsSuccessStatusCode)
+         {
+            var content = await response.Content.ReadAsStringAsync();
+            resposta = JsonSerializer.Deserialize<Resposta>(content);
+         }
+         PreencherTela();
+
+      }
+        catch (Exception e)
+      {
+          System.Diagnostics.Debug.WriteLine(e);
+      }
+   }
+
 void PreencherTela()
 {
   LabelTemp.Text= resposta.results.Temp. ToString();
+
   LabelCity.Text= resposta.results.City;
-  LabelDescription.Text= resposta.results.Description.ToString();
-  LabelRain .Text= resposta.results.Rain;
-  LabelSunrise.Text= resposta.results.Sunrise;
-  LabelHumidity.Text= resposta.results.Humidity;
-   LabelHumidity.Text= resposta.results.Humidity;
-}
+
+  LabelDescricao.Text= resposta.results.Description.ToString();
+
+  LabelChuva.Text= resposta.results.Rain.ToString();
+
+  LabelaNascerdosol.Text= resposta.results.Sunrise;
+
+  LabelUmidade.Text= resposta.results.Humidity.ToString();
+
+
+  
+   }
+  }
